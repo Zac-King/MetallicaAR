43f5428a931b33481c138599aa0f2c1e9d50fe79
Eric Mouledoux <EricMouledoux@gmail.com>
2017-04-13 11:48:12 -0500

    Password, splash, and icon

---
+++ Assets/Scripts/Password.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+[RequireComponent(typeof(InputField))]
+public class Password : MonoBehaviour
+{
+    private InputField m_Input
+    { get { return GetComponent<InputField>(); } }
+
+    [SerializeField]
+    private string m_Password;
+
+    public UnityEngine.Events.UnityEvent OnCorrectPassword;
+    
+    public void CheckPassword()
+    {
+        if (m_Input.text == m_Password)
+            OnCorrectPassword.Invoke();
+    }
+
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
